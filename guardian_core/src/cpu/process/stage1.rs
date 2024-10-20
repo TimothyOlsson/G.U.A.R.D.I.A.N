4f5be3ae82011a7730eb599961308cf224dbcdbb
TimothyBergstrom <timothy.bergstrom@gmail.com>
2024-10-20 12:20:00 +0200

    Fixed multiple writes Stage 1 when not connected + offset
    * Also changed so some calculations are only done when conneced
    * Fixed offset, wrote to the wrong location

--- guardian_core/src/cpu/process/stage1.rs
+++ guardian_core/src/cpu/process/stage1.rs
@@ -1,8 +1,8 @@
 use std::time::Instant;
 
-use tracing::{trace, debug};
+use tracing::{trace, info};
 use ndarray::{Array1, Axis};
-use itertools::multizip;
+use itertools::{any, multizip};
 use ndarray::parallel::prelude::*;
 use rayon::iter::ParallelBridge;
 
@@ -14,7 +14,7 @@ const NEURON_STATE_A: usize = 0;
 const NEURON_STATE_B: usize = 1;
 const NODE_A: usize = 2;
 const NODE_B: usize = 3;
-const DELTA_NODE_A: usize = 0;
+const DELTA_NODE: usize = 0;
 
 pub fn update_interconnected_nodes(network: &mut Network) {
     let nodes = &network.state.nodes;
@@ -27,7 +27,7 @@ pub fn update_interconnected_nodes(network: &mut Network) {
 
     let zipped = multizip(
         (
-            neuron_states.rows(),
+            neuron_states.outer_iter(),
             nodes.axis_iter(Axis(0)),
             inter_connections_source.axis_iter(Axis(0))
         )
@@ -47,14 +47,15 @@ pub fn update_interconnected_nodes(network: &mut Network) {
         let precalculated_forward = [&precalculated_forward];
         let precalculated_backward = model.precalculate(NEURON_STATE_B, neuron_state.view());
         let precalculated_backward = [&precalculated_backward];
-        for (node_a_local_index, node_a) in node_states.rows().into_iter().enumerate() {
+        for (node_a_local_index, node_a) in node_states.outer_iter().enumerate() {
             let mut node_a = unpack_array(node_a);
             let connection_a = inter_connections.get(node_a_local_index).unwrap();
             let node_a_global_index = node_a_local_index + node_index_offset;
             let node_b_global_index = connection_a.get_index();
             let (neuron_b_index, node_b_local_index) = node_global_to_local_index(node_b_global_index, g_settings);
             let connection_b = get_inter_connection(neuron_b_index, node_b_local_index, inter_connections_source);
-            let (neuron_state_b,  mut node_b) = if check_is_connected(node_a_global_index, connection_b) {
+            let is_connected = check_is_connected(node_a_global_index, connection_b);
+            let (neuron_state_b,  mut node_b) = if is_connected {
                 if node_b_global_index > node_a_global_index { continue; }  // Only highest index calculates if connected
                 (
                     get_neuron_state(neuron_b_index, neuron_states),
@@ -75,29 +76,39 @@ pub fn update_interconnected_nodes(network: &mut Network) {
                 (NODE_A, expand(node_a.view())),
                 (NODE_B, expand(node_b.view()))
             ];
-            let delta_node_a = &model.forward_from_precalc(&inputs, &precalculated_forward)[DELTA_NODE_A];
-
-            let inputs = [
-                (NEURON_STATE_A, expand(neuron_state_b.view())),
-                (NODE_A, expand(node_b.view())),
-                (NODE_B, expand(node_a.view()))
-            ];
-            let delta_node_b = &model.forward_from_precalc(&inputs, &precalculated_backward)[DELTA_NODE_A];
-
+            let delta_node_a = &model.forward_from_precalc(&inputs, &precalculated_forward)[DELTA_NODE];
             node_a = node_a + squeeze(delta_node_a.view());
-            node_b = node_b + squeeze(delta_node_b.view());
+
+            // If not connected, this could be skipped
+            if is_connected {
+                let inputs = [
+                    (NEURON_STATE_A, expand(neuron_state_b.view())),
+                    (NODE_A, expand(node_b.view())),
+                    (NODE_B, expand(node_a.view()))
+                ];
+                let delta_node_b = &model.forward_from_precalc(&inputs, &precalculated_backward)[DELTA_NODE];
+                node_b = node_b + squeeze(delta_node_b.view());
+
+            }
 
             let node_a = pack_array(node_a);
             let node_b = pack_array(node_b);
 
             // Allows for multiple process to work with the same vector without locking
             let ptr = nodes.as_ptr() as *mut u8;
-            let to_write = [
-                (node_a_global_index, node_a),
-                (node_b_global_index, node_b)
-            ];
+            let to_write = if is_connected {
+                vec![
+                    (node_a_global_index, node_a),
+                    (node_b_global_index, node_b)
+                ]
+            } else {
+                vec![
+                    (node_a_global_index, node_a),
+                ]
+            };
             let row_size = g_settings.node_size;
-            for (offset, node_state) in to_write {
+            for (node_global_index, node_state) in to_write {
+                let offset = node_global_index * row_size;
                 let data = &node_state.as_slice().unwrap();
                 unsafe { non_locking_write(ptr, offset, row_size, data); }
             }
