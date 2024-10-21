6b0c70971a85f8860feb1049209ea310d1909931
TimothyBergstrom <timothy.bergstrom@gmail.com>
2024-10-21 21:37:23 +0200

    Test of competition between intra connections

--- guardian_core/src/cpu/process/stage5.rs
+++ guardian_core/src/cpu/process/stage5.rs
@@ -19,7 +19,7 @@ const PUSHBACK: usize = 4;
 pub fn update_intra_connections(network: &mut Network) {
     let nodes = &network.state.nodes;
     let neuron_states = &network.state.neuron_states;
-    let intra_connections = &network.state.intra_connections;
+    let intra_connections = &mut network.state.intra_connections;
     let genome = &network.genome;
     let model = &genome.intraconnections_update;
     let g_settings = &network.g_settings;
@@ -28,7 +28,7 @@ pub fn update_intra_connections(network: &mut Network) {
         (
             neuron_states.rows(),
             nodes.axis_iter(Axis(0)),
-            intra_connections.axis_iter(Axis(0)),
+            intra_connections.axis_iter_mut(Axis(0)),
         )
     );
 
@@ -38,10 +38,12 @@ pub fn update_intra_connections(network: &mut Network) {
     .enumerate()
     .par_bridge()
     .into_par_iter()
-    .for_each(|(neuron_index, (neuron_state, node_states, intra_connections))| {
+    .for_each(|(_neuron_index, (neuron_state, node_states, mut intra_connections))| {
         let neuron_state = unpack_array(neuron_state);
-        let precalculated_neuron_state = model.precalculate(NEURON_STATE, neuron_state.view());
         let node_states = unpack_array(node_states.view());
+        let intra_connections_prev = intra_connections.to_owned();  // TODO: Refactor to skip this clone
+
+        let precalculated_neuron_state = model.precalculate(NEURON_STATE, neuron_state.view());
         let mut will_move_connection = Array2::from_elem(
             (g_settings.n_nodes, g_settings.n_intraconnections_per_node),
             false
@@ -52,35 +54,67 @@ pub fn update_intra_connections(network: &mut Network) {
                 &precalculated_neuron_state,
                 &precalculated_node
             ];
-            let already_connected: Vec<usize> = intra_connections.row(node_a_local_index).iter().map(|c| c.get_index()).collect();
-            for (connection_index, connection) in intra_connections.row(node_a_local_index).iter().enumerate() {
-                let main_gradient = update_main_connection(connection, model, &precalculated, &node_states);
-                let pending_gradient = update_pending_connection(connection, model, &precalculated, &node_states, intra_connections.view(), g_settings);
+            let mut node_intra_connections = intra_connections.row_mut(node_a_local_index);
+            let already_connected = node_intra_connections.iter().map(|c| c.get_index()).collect();
+            for (connection_index, connection) in node_intra_connections.iter_mut().enumerate() {
+                let main_gradient = update_main_connection(
+                    connection,
+                    model,
+                    &precalculated,
+                    &node_states
+                );
+                let pending_gradient = update_pending_connection(
+                    &already_connected,
+                    connection,
+                    model,
+                    &precalculated,
+                    &node_states,
+                    &intra_connections_prev,
+                    g_settings
+                );
+
+                // TODO: Is this a good idea?
                 if main_gradient > pending_gradient {
-                    connection.reset_pending();  // TODO: Is this a good idea?
+                    connection.reset_pending();
                     continue;
                 }
 
-                // Do not connect to nodes already connected with this node
-                // TODO: Can this be done in a better way?
-                let pending_index = connection.get_pending_index();
-                // TODO: Doublecheck that this works, something is off
-                if already_connected.contains(&pending_index) {
-                    continue;
-                }
-
-                let pending_bond_force = connection.get_pending_connection_bond_force();
-                let bond_force = connection.get_connection_bond_force();
+                let pending_bond_force = connection.get_pending_bond_force();
+                let bond_force = connection.get_bond_force();
                 if pending_bond_force > bond_force {
                     //debug!("Bond force for Neuron {neuron_index} Node {node_a_local_index} pending bond force {pending_bond_force} > bond force {bond_force}");
                     *will_move_connection.get_mut((node_a_local_index, connection_index)).unwrap() = true;
                 }
             }
+
+            // TODO: Make this cleaner
+            // TODO: Should this be done?
+            for connection_index_a in 0..g_settings.n_intraconnections_per_node {
+                for connection_index_b in 0..g_settings.n_intraconnections_per_node {
+                    if connection_index_a == connection_index_b { continue }
+                    let connection_a = node_intra_connections.get(connection_index_a).unwrap();
+                    let connection_b = node_intra_connections.get(connection_index_b).unwrap();
+
+                    // A bit messy logic
+                    if connection_a.pending_index == connection_b.pending_index {
+                        if connection_a.get_pending_bond_force() > connection_b.get_pending_bond_force() {
+                            node_intra_connections.get_mut(connection_index_b).unwrap().reset_pending();
+                            *will_move_connection.get_mut([node_a_local_index, connection_index_b]).unwrap() = false;
+                        } else {
+                            node_intra_connections.get_mut(connection_index_a).unwrap().reset_pending();
+                            *will_move_connection.get_mut([node_a_local_index, connection_index_a]).unwrap() = false;
+                        }
+
+                    }
+
+                }
+            }
+
         }
 
         // Need to do this last, since otherwise the index will move during the search
         for node_a_local_index in 0..g_settings.n_nodes {
-            for (connection_index, connection) in intra_connections.row(node_a_local_index).iter().enumerate() {
+            for (connection_index, connection) in intra_connections.row_mut(node_a_local_index).iter_mut().enumerate() {
                 let will_move = *will_move_connection.get((node_a_local_index, connection_index)).unwrap();
                 if will_move {
                     //debug!("Moving Neuron {neuron_index} Node {node_a_local_index} connection {connection_index} to Node {}", connection.get_pending_index());
@@ -95,7 +129,7 @@ pub fn update_intra_connections(network: &mut Network) {
 
 
 fn update_main_connection(
-    connection: &IntraConnection,
+    connection: &mut IntraConnection,
     model: &Model,
     precalculated: &[&Array1<f32>],
     nodes: &Array2<f32>,
@@ -119,11 +153,12 @@ fn update_main_connection(
 /// Search neurons side-by-side and the connecting neuron. Same there
 /// TODO: Split function? Very big input
 fn update_pending_connection(
-    connection: &IntraConnection,
+    already_connected: &Vec<usize>,
+    connection: &mut IntraConnection,
     model: &Model,
     precalculated: &[&Array1<f32>],
     nodes: &Array2<f32>,
-    intra_connections: ArrayView2<IntraConnection>,
+    intra_connections: &Array2<IntraConnection>,
     g_settings: &GuardianSettings,
 ) -> f32 {
     let pending_node_index = connection.get_pending_index();
@@ -131,53 +166,12 @@ fn update_pending_connection(
     let mut params_highest_gradient = ConnectionParams { strength: 0.0, pushback: 0.0, gradient: f32::MIN };
 
     // Start with searching neuron and vicinity where pending is index
-    search_area(
-        &mut index_highest_gradient,
-        &mut params_highest_gradient,
-        pending_node_index,
-        pending_node_index,
-        connection,
-        model,
-        precalculated,
-        nodes,
-        g_settings
-    );
-
-    // Search connected nodes
-    for connection in intra_connections.row(pending_node_index) {
-        let node_b_index = connection.get_index();
-        let node_b = nodes.slice(s![node_b_index, ..]);
-        let inputs = [
-            (NODE_B, expand(node_b.view()))
-            // Strength and pushback is always 0!
-        ];
-        let output = model.forward_from_precalc(&inputs, precalculated);
-        let connection_params = unpack_connection_model_output(output);
-        if connection_params.gradient > params_highest_gradient.gradient {
-            params_highest_gradient = connection_params;
-            index_highest_gradient = node_b_index;
-        }
-    }
-    connection.store_pending_strength_and_pushback(params_highest_gradient.strength, params_highest_gradient.pushback);
-    connection.store_pending_index(index_highest_gradient);
-    params_highest_gradient.gradient.max(0.0)
-}
-
-fn search_area(
-    index_highest_gradient: &mut usize,
-    params_highest_gradient: &mut ConnectionParams,
-    start_node_index: usize,
-    pending_node_index: usize,
-    connection: &IntraConnection,
-    model: &Model,
-    precalculated: &[&Array1<f32>],
-    nodes: &Array2<f32>,
-    g_settings: &GuardianSettings,
-) {
-    // Search nodes in close proximity
     let node_range = -(g_settings.n_intraconnected_nodes_search as isize)..=(g_settings.n_intraconnected_nodes_search as isize);
     for node_offset in node_range {
-        let node_b_index = wrap_index(start_node_index, node_offset, g_settings.n_nodes);
+        let node_b_index = wrap_index(pending_node_index, node_offset, g_settings.n_nodes);
+
+        if already_connected.contains(&node_b_index) { continue }
+
         let node_b = nodes.slice(s![node_b_index, ..]);
         let is_pending = pending_node_index == node_b_index;
         let (strength, pushback) = if is_pending {
@@ -198,8 +192,31 @@ fn search_area(
         connection_params.strength += strength;
         connection_params.pushback += pushback;
         if connection_params.gradient > params_highest_gradient.gradient {
-            *params_highest_gradient = connection_params;
-            *index_highest_gradient = node_b_index;
+            params_highest_gradient = connection_params;
+            index_highest_gradient = node_b_index;
         }
     }
+
+    // Search connected nodes
+    for connection in intra_connections.row(pending_node_index) {
+        let node_b_index = connection.get_index();
+
+        if already_connected.contains(&node_b_index) { continue }
+
+        let node_b = nodes.slice(s![node_b_index, ..]);
+        let inputs = [
+            (NODE_B, expand(node_b.view()))
+            // Strength and pushback is always 0!
+        ];
+
+        let output = model.forward_from_precalc(&inputs, precalculated);
+        let connection_params = unpack_connection_model_output(output);
+        if connection_params.gradient > params_highest_gradient.gradient {
+            params_highest_gradient = connection_params;
+            index_highest_gradient = node_b_index;
+        }
+    }
+    connection.store_pending_strength_and_pushback(params_highest_gradient.strength, params_highest_gradient.pushback);
+    connection.store_pending_index(index_highest_gradient);
+    params_highest_gradient.gradient.max(0.0)
 }
